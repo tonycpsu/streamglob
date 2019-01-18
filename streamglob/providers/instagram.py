d3d2f84477aa1ab8d55dc7cb4947deea0168baea
Tony Cebzanov <tonycpsu@gmail.com>
2019-01-18 16:03:14 -0500

    Proxy support in InstagramProvider.
    * instagram_private_api uses its own HTTPS client, so we can't use
      StreamSession/requests.
    * Fortunately, it includes some support for using a proxy, albeit with a
      warning about proxy support being an "alpha" feature.
    * Anyway proxy support seems to work now.  Whee!

--- streamglob/providers/instagram.py
+++ streamglob/providers/instagram.py
@@ -1,14 +1,91 @@
-from .feed import *
+import warnings
 
+from .feed import *
 from ..exceptions import *
 from ..state import *
 from .. import config
 from .. import model
-
+from .. import session
 from .filters import *
 
+from orderedattrdict import DefaultAttrDict
 from instagram_web_api import Client, ClientCompatPatch, ClientError
 from pony.orm import *
+
+class InstagramSession(session.StreamSession):
+
+    BATCH_COUNT = 25
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args, **kwargs
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.web_api = Client(
+                proxy=self.proxies.get("https"),
+                auto_patch=True, drop_incompat_keys=False
+            )
+        self.end_cursors = DefaultAttrDict(lambda: None)
+
+    @memo(region="long")
+    def user_name_to_id(self, user_name):
+        try:
+            user_id = self.web_api.user_info2(user_name)["id"]
+        except:
+            raise SGException(f"user id for {user_name} not found")
+        return user_id
+
+    def get_feed_items(self, user_name, count=BATCH_COUNT):
+
+        feed = self.web_api.user_feed(
+            self.user_name_to_id(user_name), count=self.BATCH_COUNT,
+            end_cursor = self.end_cursors[user_name]
+        )
+
+        for post in feed:
+            try:
+                cursor = (
+                    post["node"]["edge_media_to_comment"]
+                    ["page_info"]["end_cursor"]
+                )
+                if cursor:
+                    self.end_cursors[user_name] = cursor
+            except KeyError:
+                pass
+
+            post_id = post["node"]["id"]
+            try:
+                subject = post["node"]["caption"]["text"].replace("\n", "")
+            except TypeError:
+                subject = "(no caption)"
+
+            media_type = post["node"]["type"]
+            if media_type == "video":
+                url = post["node"]["link"]
+            elif media_type == "image":
+                if "carousel_media" in post["node"]:
+                    url = [ m["images"]["standard_resolution"]["url"]
+                            for m in post["node"]["carousel_media"] ]
+                else:
+                    url = post["node"]["images"]["standard_resolution"]["url"]
+            else:
+                logger.warn(f"no content for post {post_id}")
+                continue
+
+            yield(
+                FeedItem(
+                    guid = post_id,
+                    subject = subject,
+                    created = datetime.fromtimestamp(
+                        int(post["node"]["created_time"])
+                    ),
+                    media_type = media_type,
+                    url = url
+                )
+            )
+
+
 
 class InstagramItem(model.MediaItem):
 
@@ -18,87 +95,46 @@ class InstagramFeed(model.MediaFeed):
 
     ITEM_CLASS = InstagramItem
 
+    @db_session
     def update(self, limit = None):
 
-        if not limit:
-            limit = self.DEFAULT_ITEM_LIMIT
-
-        self.end_cursor = None
-
-        if self.name.startswith("@"):
+        if self.locator.startswith("@"):
             user_name = self.name[1:]
         else:
             raise NotImplementedError
 
-        try:
-            user_id = self.provider.web_api.user_info2(user_name)["id"]
-        except:
-            raise SGException(f"user id for {user_name} not found")
+        if not limit:
+            limit = self.DEFAULT_ITEM_LIMIT
 
-
-        # logger.info(self.end_cursor)
         last_count = 0
         count = 0
 
         while(count < limit):
             # instagram API will sometimes give duplicates using end_cursor
             # for pagination, so instead of specifying how many posts to get,
-            # we just get 25 at a time and break the loop after we've gotten
-            # the desired number of posts, or after a batch entirely comprised
-            # of duplicates
-            feed = self.provider.web_api.user_feed(
-                user_id, count=25,
-                end_cursor = self.end_cursor
-            )
-            for post in feed:
-                try:
-                    cursor = (
-                        post["node"]["edge_media_to_comment"]
-                        ["page_info"]["end_cursor"]
+            # we just get a batch of them at a time and break the loop after
+            # we've gotten the desired number of posts, or after a batch
+            # entirely comprised of duplicates
+            for post in self.session.get_feed_items(user_name):
+                i = self.items.select(lambda i: i.guid == post.guid).first()
+                if not i:
+                    i = self.ITEM_CLASS(
+                        feed = self,
+                        guid = post.guid,
+                        subject = post.subject,
+                        created = post.created,
+                        media_type = post.media_type,
+                        content = post.url
                     )
-                    if cursor:
-                        self.end_cursor = cursor
-                except KeyError:
-                    pass
-
-                post_id = post["node"]["id"]
-                if post_id not in select(i.guid for i in self.items.select())[:]:
-
-                    try:
-                        subject = post["node"]["caption"]["text"].replace("\n", "")
-                    except TypeError:
-                        subject = "(no caption)"
-
                     count += 1
-                    media_type = post["node"]["type"]
-                    if media_type == "video":
-                        url = post["node"]["link"]
-                    elif media_type == "image":
-                        if "carousel_media" in post["node"]:
-                            url = [ m["images"]["standard_resolution"]["url"]
-                                    for m in post["node"]["carousel_media"] ]
-                        else:
-                            url = post["node"]["images"]["standard_resolution"]["url"]
-                    else:
-                        logger.warn(f"no content for post {post_id}")
-                        continue
 
-                    i = self.items.select(lambda i: i.guid == post_id).first()
-                    if not i:
-                        i = self.ITEM_CLASS(
-                            feed = self,
-                            guid = post_id,
-                            subject = subject,
-                            created = datetime.fromtimestamp(
-                                int(post["node"]["created_time"])
-                            ),
-                            media_type = media_type,
-                            content = url
-                    )
-            if count == last_count:
-                logger.info(f"breaking after {count}")
-                return
-            last_count = count
+                if count == last_count:
+                    logger.info(f"breaking after {count}")
+                    return
+                last_count = count
+
+
+        # logger.info(self.end_cursor)
 
 
 # class InstagramProviderView(SimpleProviderView):
@@ -115,12 +151,14 @@ class InstagramProvider(PaginatedProviderMixin, CachedFeedProvider):
 
     MEDIA_TYPES = {"image", "video"}
 
+    SESSION_CLASS = InstagramSession
+
     # VIEW_CLASS = InstagramProviderView
 
-    def __init__(self, *args, **kwargs):
-        self.web_api = Client(auto_patch=True, drop_incompat_keys=False)
-        self.end_cursor = None
-        super().__init__(*args, **kwargs)
+    # def __init__(self, *args, **kwargs):
+    #     self.web_api = Client(auto_patch=True, drop_incompat_keys=False)
+    #     self.end_cursor = None
+    #     super().__init__(*args, **kwargs)
 
     def play_args(self, selection, **kwargs):
 
