97e6e5914a6391dc270f88969b7450b77fa1218e
Tony Cebzanov <tonycpsu@gmail.com>
2020-07-19 13:08:50 -0400

    Fix error when caption is empty.

--- streamglob/providers/instagram.py
+++ streamglob/providers/instagram.py
@@ -143,7 +143,7 @@ class InstagramFeed(model.MediaFeed):
                 i = self.ITEM_CLASS(
                     feed = self,
                     guid = post.shortcode,
-                    title = post.caption.replace("\n", " ") or "(no caption)",
+                    title = (post.caption or "(no caption)").replace("\n", " "),
                     created = post.date_utc,
                     media_type = media_type,
                     content =  InstagramMediaSource.schema().dumps(
