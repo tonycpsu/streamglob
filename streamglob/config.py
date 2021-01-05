3c30663c7b1bd55c4bd85f4b1e4b5c7dd0459c3c
Tony Cebzanov <tonycpsu@gmail.com>
2021-01-04 20:10:13 -0500

    Fix exception when config dir isn't set

--- streamglob/config.py
+++ streamglob/config.py
@@ -225,7 +225,11 @@ class Config(ConfigTree):
 
 def load(config_dir=None, merge_default=False):
     global settings
-    settings = Config(os.path.expanduser(config_dir), merge_default=merge_default)
+    settings = Config(
+        os.path.expanduser(config_dir)
+        if config_dir
+        else Config.DEFAULT_CONFIG_DIR, merge_default=merge_default
+    )
 
 # settings = Config(CONFIG_FILE, merge_default=True)
 
